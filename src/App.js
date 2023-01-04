aa24bc228baeaec83ccc30e34e07e21fce67c3bc
Abhishek-data <abhishekkr1915@gmail.com>
2023-01-04 19:25:32 +0530

    Add UserInfo App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import AddUser from "./components/Users/AddUser";
+import UserList from "./components/Users/UserList";
 
 function App() {
+  const [userData, setUserData] = useState([]);
+  
+  const userDataHandler = (enteredData) => {
+    setUserData((prevData) => {
+      const updateUser = [...prevData];
+      updateUser.unshift({
+        name: enteredData.username,
+        age: enteredData.age,
+        id: Math.random().toString()
+      });
+      return updateUser;
+    });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AddUser onAddUser={userDataHandler} />
+      <UserList users={userData}/>
     </div>
   );
 }
